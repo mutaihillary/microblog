7c5aecfe5331a434a4d447c79e7cdcab4919a94a
mutaihillary <mutaihillary@yahoo.com>
2016-08-12 14:51:36 +0300

    add

---
+++ run.py
@@ -0,0 +1,14 @@
+#!flask/bin/python
+from app import app
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+#
+# @app.route('/')
+# def index():
+#     return "This is home!"
+# # start the server
+# if __name__== "__main__":
+app.run(debug=True, port=5000)
+
